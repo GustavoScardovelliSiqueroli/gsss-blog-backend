96dde6d45ea8f2aff3e5d0fde522598707446ad5
gustavo <gustavosecardovelli@gmail.com>
2024-08-20 10:09:46 -0300

    test: test commit pylance

--- app/domain/models/post.py
+++ app/domain/models/post.py
@@ -6,3 +6,11 @@ class Post(Base):
     __tablename__ = "posts"
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String(50))
+
+
+def teste(teste: str) -> str:
+    return teste
+
+
+teste(True)
+teste({"ola": "teste"})
