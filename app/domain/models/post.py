050917756ce730956632a5d9894ab9cf0595cb3d
gustavo <gustavosecardovelli@gmail.com>
2024-08-21 10:58:58 -0300

    fix: string to text in content post

--- app/domain/models/post.py
+++ app/domain/models/post.py
@@ -1,10 +1,10 @@
-from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy import Column, Integer, String, DateTime, Text
 from app.infra.db.database import Base
 
 
 class Post(Base):
     __tablename__ = "posts"
     id = Column(Integer, primary_key=True, index=True)
-    title = Column(String(50))
-    content = Column(String(500))
-    created_at = Column(DateTime)
+    title = Column(String(50), nullable=False)
+    content = Column(Text, nullable=False)
+    created_at = Column(DateTime, nullable=False)
